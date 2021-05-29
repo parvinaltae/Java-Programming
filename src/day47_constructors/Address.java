a603c7f4ae3b321d7e5af511072b0853ce2ddf4f
Parvin <parvin.altae@gmail.com>
2021-05-29 10:19:46 -0400

    day47 code

--- src/day47_constructors/Address.java
+++ src/day47_constructors/Address.java
@@ -1,16 +1,43 @@
 package day47_constructors;
 
 public class Address {
-    private String street, city, state;
-    private int zipCode;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
     private String country = "USA";
+    //constructor method- automatically called
+    public Address() {
+        System.out.println("Address constructor");
+        street = "123 unknown st";
+        city = "Unknown";
+        state = "Unknown";
+        zipCode = "00000";
+    }
+    //second constructor, overloaded constructor -> provides shortcut to initialize variables
+    //in same statement: ex: Address ad = new Address("123 java st", "Boston", "MA", "43213");
+    public Address(String street, String city, String state, String zipCode) {
+        setStreet(street);//reuse the code in the setter method
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public String toString() {
+        return street + ", " + city + ", " + state + " " + zipCode;
+    }
 
     public String getStreet() {
         return street;
     }
 
     public void setStreet(String street) {
-       this.street = street;
+        if(street.isEmpty() || street.length() > 50) {
+            System.out.println("ERROR: Invalid street");
+            //System.exit(0);
+        } else {
+            this.street = street;
+        }
     }
 
     public String getCity() {
@@ -29,16 +56,19 @@ public class Address {
         this.state = state;
     }
 
-    public int getZipCode() {
+    public String getZipCode() {
         return zipCode;
     }
 
-    public void setZipCode(int zipCode) {
+    public void setZipCode(String zipCode) {
         this.zipCode = zipCode;
     }
 
-    @Override
-    public String toString() {
-        return  street+", "+ city+", "+ state + " " + "zipCode";
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
---
+++ src/day47_constructors/Student.java
@@ -0,0 +1,12 @@
+package day47_constructors;
+
+public class Student {
+    //no args constructor
+    public Student() {
+        System.out.println("No args student");
+    }
+    public Student(String name){
+        System.out.println("Name param constructor");
+
+    }
+}
